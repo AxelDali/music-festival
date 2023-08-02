7120e36a0e2bb6c53dd7212b9614ce3b3ea9fd7c
Axel Dalí <axeldali.gm@gmail.com>
2023-08-02 16:13:02 -0600

    Add and set up Gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const {src, dest, watch} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+    src('src/scss/app.scss')
+        .pipe(sass())
+        .pipe(dest('build/css'));
+    done();
+}
+
+function dev(done) {
+    watch('src/scss/app.scss', css);
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
