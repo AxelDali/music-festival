78e66c7b00f96c6a1e6ff7b077cdcea9896d98c4
Axel Dalí <axeldali.gm@gmail.com>
2023-08-02 22:02:13 -0600

    Add gulp-plumber to devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const {src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done) {
     src('src/scss/**/*.scss')
+        .pipe(plumber())
         .pipe(sass())
         .pipe(dest('build/css'));
     done();
