2c3eda11b6e498ecbce5a55fd0bb52ef98763425
Axel Dalí <axeldali.gm@gmail.com>
2023-08-04 10:15:38 -0600

    Add WEBP converter to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, parallel} = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Images
+const webp = require('gulp-webp');
 
 function css(done) {
     src('src/scss/**/*.scss')
@@ -10,10 +15,21 @@ function css(done) {
     done();
 }
 
+function convertToWebp(done) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.convertToWebp = convertToWebp;
+exports.dev = parallel(convertToWebp, dev);
