59417974624f14051209b787a847f20e1bdf872f
Axel Dalí <axeldali.gm@gmail.com>
2023-08-04 10:48:10 -0600

    Add AVIF converter to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ const plumber = require('gulp-plumber');
 const cache = require('gulp-cache')
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     src('src/scss/**/*.scss')
@@ -37,6 +38,16 @@ function convertToWebp(done) {
     done();
 }
 
+function convertToAvif(done) {
+    const options = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done) {
     watch('src/scss/**/*.scss', css);
     done();
@@ -45,4 +56,5 @@ function dev(done) {
 exports.css = css;
 exports.images = images;
 exports.convertToWebp = convertToWebp;
-exports.dev = parallel(images, convertToWebp, dev);
+exports.convertToAvif = convertToAvif;
+exports.dev = parallel(images, convertToWebp, convertToAvif, dev);
