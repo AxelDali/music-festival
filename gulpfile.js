5d5c64fb9033d180044d183f9eb1293c0fb22541
Axel Dalí <axeldali.gm@gmail.com>
2023-08-07 12:31:36 -0600

    Minify JavaScript code

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,9 @@ const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
+// JavaScript
+const terser = require('gulp-terser-js');
+
 function css(done) {
     src('src/scss/**/*.scss')
         .pipe(sourcemaps.init())
@@ -57,6 +60,9 @@ function convertToAvif(done) {
 
 function javascript(done) {
     src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
     done();
 }
