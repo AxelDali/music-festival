f25b92e6e045bad2eb55319028d4b03bb77bccc1
Axel Dalí <axeldali.gm@gmail.com>
2023-08-06 23:16:43 -0600

    Add script for modal window in gallery

--- build/js/app.js
+++ build/js/app.js
@@ -15,6 +15,41 @@ function createGallery() {
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
             <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="Imagen Galería">
         `;
+        image.onclick = function () {
+            showImage(i);
+        };
         gallery.appendChild(image);
     }
+}
+
+function showImage(id) {
+    const image = document.createElement('picture');
+    image.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen Galería">
+    `;
+    // Creates overlay
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(image);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fix-body');
+        overlay.remove();
+    }
+    // Close button for modal
+    const closeModal = document.createElement('P');
+    closeModal.textContent = 'X';
+    closeModal.classList.add('btn-close');
+    closeModal.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fix-body');
+        overlay.remove();
+    };
+    overlay.appendChild(closeModal);
+    // Appends overlay to HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+    body.classList.add('fix-body');
 }
