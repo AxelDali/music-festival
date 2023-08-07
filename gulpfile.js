daf1f1294d6fc527dd4fcb7de9de76455a79ba84
Axel Dalí <axeldali.gm@gmail.com>
2023-08-07 12:25:53 -0600

    Minify CSS code

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const {src, dest, watch, parallel} = require('gulp');
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Images
 const cache = require('gulp-cache')
@@ -12,8 +16,11 @@ const avif = require('gulp-avif');
 
 function css(done) {
     src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(plumber())
         .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));
     done();
 }
